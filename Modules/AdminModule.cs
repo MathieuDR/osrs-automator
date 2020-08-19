77febc541e49969e312fba25f85f20ecdf9916bd
Mathieu De Raedt <mathieu.deraedt@caldr.com>
2020-08-19 16:19:03 +0200

    [ADD] Joined List. Requested

--- Modules/AdminModule.cs
+++ Modules/AdminModule.cs
@@ -1,7 +1,10 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
 using DiscordBotFanatic.Models.Enums;
 using DiscordBotFanatic.Services.interfaces;
 
@@ -15,6 +18,38 @@ namespace DiscordBotFanatic.Modules {
 
         public AdminModule(IGuildService guildService) {
             _guildService = guildService;
+        }
+
+        [Name("Joined at list")]
+        [Command("joinedList")]
+        [Summary("print a CSV list of user & Joined date")]
+        public async Task PrintJoinedList() {
+
+            List<string> messages = new List<string>();
+            StringBuilder csv = new StringBuilder();
+
+            csv.AppendLine("User;nickname;JoinedAt;isbot");
+            foreach (SocketGuildUser guildUser in Context.Guild.Users) {
+                var line =
+                    $"{guildUser.Username}#{guildUser.Discriminator};{guildUser.Nickname};{guildUser.JoinedAt};{guildUser.IsBot}";
+
+                if (csv.Length + line.Length >= 2000) {
+                    messages.Add(csv.ToString());
+                    csv = new StringBuilder();
+                }
+
+                csv.AppendLine(line);
+            }
+
+            messages.Add(csv.ToString());
+
+            EmbedBuilder builder = new EmbedBuilder();
+            builder.Title = $"Success";
+            await ReplyAsync(embed:builder.Build());
+
+            foreach (string message in messages) {
+                await ReplyAsync(message);
+            }
         }
 
         [Name("Toggle Permission")]
