b4c30aa3d51e75e3b657c108da0d0ef1d0ea889b
Mathieu De Raedt <mathieuderaedt@gmail.com>
2021-06-12 16:23:07 +0200

    Finish scraper

--- WOMReader/Program.cs
+++ WOMReader/Program.cs
@@ -1,63 +1,200 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using DiscordBotFanatic.Models.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Context;
 using Serilog.Events;
 using Serilog.Formatting.Json;
 using WiseOldManConnector.Configuration;
+using WiseOldManConnector.Helpers;
 using WiseOldManConnector.Interfaces;
-using WiseOldManConnector.Models;
-using WiseOldManConnector.Models.Output;
 using WiseOldManConnector.Models.WiseOldMan.Enums;
 
 namespace WOMReader {
-    class Program {
-        public async Task EntryPointAsync() {
-            IConfiguration config = BuildConfig();
-            IServiceProvider services = ConfigureServices(config);
+    public class AggregratedDelta {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Dictionary<MetricType, double> Gained { get; } = new();
+        public DateTimeOffset Start { get; set; }
+        public DateTimeOffset End { get; set; }
+        public TimeSpan Duration => End - Start;
+    }
 
-            
+    internal class Program {
+        public async Task EntryPointAsync() {
+            var config = BuildConfig();
+            var services = ConfigureServices(config);
+
+            var metrics = new List<MetricType> {
+                MetricType.Overall,
+                MetricType.EffectiveHoursBossing, 
+                MetricType.EffectiveHoursPlaying,
+                MetricType.TheGauntlet,
+                MetricType.TheCorruptedGauntlet,
+                MetricType.Zalcano,
+                MetricType.Thieving,
+                MetricType.Zulrah,
+                MetricType.ChambersOfXeric,
+                MetricType.ChambersOfXericChallengeMode,
+                MetricType.GeneralGraardor,
+                MetricType.CommanderZilyana,
+                MetricType.KrilTsutsaroth,
+                MetricType.Kreearra,
+                MetricType.BarrowsChests,
+                MetricType.Sarachnis,
+                MetricType.CorporealBeast,
+                MetricType.Nightmare,
+                MetricType.GrotesqueGuardians,
+                MetricType.Obor, 
+                MetricType.Bryophyta,
+                MetricType.Hespori,
+                MetricType.Slayer,
+                MetricType.AlchemicalHydra,
+                MetricType.Herblore,
+                MetricType.Mining,
+                MetricType.Agility,
+                MetricType.Fishing,
+                MetricType.Hunter,
+                MetricType.Runecrafting
+
+            };
+
+
             var logger = services.GetRequiredService<ILogger<Program>>();
             var groupApi = services.GetRequiredService<IWiseOldManGroupApi>();
             var compApi = services.GetRequiredService<IWiseOldManCompetitionApi>();
             var womConfig = services.GetRequiredService<WiseOldManConfiguration>();
 
-            var competitionsResponse = await groupApi.Competitions(womConfig.GroupId);
-            var competitions = competitionsResponse.Data.OrderByDescending(c=>c.StartDate).ToList();
+            using (LogContext.PushProperty("Group", womConfig.GroupId)) {
+                var competitionsResponse = await groupApi.Competitions(womConfig.GroupId);
+                var competitions = competitionsResponse.Data.Where(x=> x.EndDate <= DateTimeOffset.Now).OrderByDescending(c => c.StartDate).ToList();
 
-            foreach (var competition in competitions) {
-                logger.LogInformation($"Competition {competition.Title} - {competition.Metric}");
-                logger.LogInformation($"Time {competition.StartDate} - {competition.EndDate} ({competition.Duration})");
-                logger.LogInformation("_____________________");
+                var dict = new Dictionary<string, List<AggregratedDelta>>();
 
-                var ehb = await compApi.View(competition.Id, MetricType.EffectiveHoursBossing);
-                ConnectorResponse<Competition> ehp = await compApi.View(competition.Id, MetricType.EffectiveHoursPlaying);
-                
-                logger.LogInformation($"EHB Players {ehb.Data.ParticipantCount}, gained {ehb.Data.TotalGained}");
-                logger.LogInformation($"EHP Players {ehp.Data.ParticipantCount}, gained {ehp.Data.TotalGained}");
+                for (var index = 0; index < competitions.Count; index++) {
+                    var competition = competitions[index];
+                    using (LogContext.PushProperty("competition", new {competition.Id, competition.Metric, competition.Title, competition.Duration, competition.StartDate, competition.EndDate})) {
+                        logger.LogInformation($"Competition {competition.Title} - {competition.Metric}");
+                        logger.LogInformation(
+                            $"Time {competition.StartDate} - {competition.EndDate} ({competition.Duration})");
+
+
+                        foreach (var metric in metrics) {
+                            var info = await compApi.View(competition.Id, metric);
+                            logger.LogInformation(
+                                $"{metric.ToString()} Players {info.Data.ParticipantCount}, gained {info.Data.TotalGained}");
+
+                            for (var i = 0; i < info.Data.Participants.Count; i++) {
+                                var participant = info.Data.Participants[i];
+
+                                logger.LogInformation(
+                                    $"{metric.ToString()} {participant.Player.Username}, gained {participant.CompetitionDelta.Gained}");
+
+                                double treshold = metric.Category() switch {
+                                    MetricTypeCategory.Skills => 10000,
+                                    MetricTypeCategory.Bosses => 5,
+                                    MetricTypeCategory.Activities => 10,
+                                    MetricTypeCategory.Time => 1,
+                                    _ => throw new ArgumentOutOfRangeException()
+                                };
+
+                                if (participant.CompetitionDelta.Gained <= treshold) {
+                                    logger.LogInformation(
+                                        $"{metric.ToString()} {participant.Player.Username}, inactive");
+                                    continue;
+                                }
+
+
+                                if (!dict.TryGetValue(participant.Player.Username, out var list)) {
+                                    list = new List<AggregratedDelta>();
+                                    dict.Add(participant.Player.Username, list);
+                                }
+
+                                var delta = list.FirstOrDefault(x => x.Id == info.Data.Id);
+                                if (delta is null) {
+                                    delta = new AggregratedDelta();
+                                    delta.End = info.Data.EndDate;
+                                    delta.Start = info.Data.StartDate;
+                                    delta.Name = info.Data.Title;
+                                    delta.Id = info.Data.Id;
+                                    list.Add(delta);
+                                }
+
+                                delta.Gained.Add(metric, participant.CompetitionDelta.Gained);
+                            }
+
+                        }
+                        logger.LogInformation($"Sleep one minute");
+                        Thread.Sleep(60*1000);
+                        logger.LogInformation($"Waking");
+                    }
+
+                    logger.LogInformation("__________________________________________");
+                    
+                    
+                }
+
+                logger.LogInformation("__________________________________________");
+
+
+                foreach (var kvp in dict.OrderBy(x => x.Key)) {
+                    logger.LogInformation("__________________________________________");
+
+                    var totalDuration = TimeSpan.Zero;
+                    var metricDuration = new TimeSpan[metrics.Count];
+                    var gained = new double[metrics.Count];
+
+                    foreach (var aggregratedDelta in kvp.Value) {
+                        totalDuration = totalDuration.Add(aggregratedDelta.Duration);
+
+                        foreach (var gainedKvp in aggregratedDelta.Gained) {
+                            var i = metrics.IndexOf(gainedKvp.Key);
+                            gained[i] += gainedKvp.Value;
+                            
+                            metricDuration[i] = metricDuration[i].Add(aggregratedDelta.Duration);
+                        }
+                    }
+
+                    logger.LogInformation($"{kvp.Key}, {Math.Round(totalDuration.TotalDays, 2)} days over whole active period");
+
+
+                    for (var i = 0; i < metrics.Count; i++) {
+                        var metric = metrics[i];
+                        if(gained[i] > 0) {
+                            logger.LogInformation(
+                                $"{metric}, TTL: {Math.Round(gained[i], 2)}, AVG: {Math.Round(gained[i] / metricDuration[i].TotalDays, 2)} for {Math.Round(metricDuration[i].TotalDays, 2)} days");
+                        } else {
+                            logger.LogInformation(
+                                $"{metric}, inactive");
+                        }
+                    }
+                }
+
+                logger.LogInformation("Press enter to exit");
+                Console.ReadLine();
             }
-            
-            logger.LogInformation("Press enter to exit");
-            Console.ReadLine();
         }
-        
-        private static void Main() => new Program().EntryPointAsync().GetAwaiter().GetResult();
-        
-         private IServiceProvider ConfigureServices(IConfiguration config) {
-            
-            WiseOldManConfiguration manConfiguration = config.GetSection("WiseOldMan").Get<WiseOldManConfiguration>();
-        
+
+        private static void Main() {
+            new Program().EntryPointAsync().GetAwaiter().GetResult();
+        }
+
+        private IServiceProvider ConfigureServices(IConfiguration config) {
+            var manConfiguration = config.GetSection("WiseOldMan").Get<WiseOldManConfiguration>();
+
             Log.Logger = new LoggerConfiguration()
                 .Enrich.FromLogContext()
                 .MinimumLevel
                 .Debug()
                 .WriteTo.File(new JsonFormatter(), "logs/osrs_console.log")
-                .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information)
+                .WriteTo.Console(LogEventLevel.Information)
                 .CreateLogger();
 
             return new ServiceCollection()
@@ -73,7 +210,7 @@ namespace WOMReader {
             return new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
-                .AddJsonFile($"appsettings.{environmentName}.json", optional: true).Build();
+                .AddJsonFile($"appsettings.{environmentName}.json", true).Build();
         }
     }
 }
