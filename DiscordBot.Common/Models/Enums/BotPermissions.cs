80ed529976d80d67dd19d97b106320ae41d8ccc9
Mathieu De Raedt <mathieu@deraedt.dev>
2021-08-28 13:03:43 +0200

    Small bugfixes

--- DiscordBot.Common/Models/Enums/BotPermissions.cs
+++ DiscordBot.Common/Models/Enums/BotPermissions.cs
@@ -3,8 +3,8 @@
 namespace DiscordBot.Common.Models.Enums {
     [Flags]
     public enum BotPermissions {
-        None = 0,
-        EventManager,
-        CompetitionManager
+        None = 1,
+        EventManager = 2,
+        CompetitionManager = 4
     }
 }
