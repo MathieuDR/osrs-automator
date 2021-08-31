e5ec51e2370b74e1701ce3a03afd79ff9c6fe9c0
Mathieu De Raedt <mathieu@deraedt.dev>
2021-08-31 23:40:39 +0200

    remove unneeded comments

--- DiscordBot/Services/InteractiveCommandHandlerService.cs
+++ DiscordBot/Services/InteractiveCommandHandlerService.cs
@@ -78,11 +78,6 @@ namespace DiscordBot.Services {
 
                 await _client.Rest.CreateGuildCommand(command, 403539795944538122);
             } catch (ApplicationCommandException e) {
-                // If our command was invalid, we should catch an ApplicationCommandException. This exception contains the path of the error as well as the error message. You can serialize the Error field in the exception to get a visual of where your error is.
-                //var json = JsonConvert.SerializeObject(e.Error, Formatting.Indented);
-
-                // You can send this error somewhere or just print it to the console, for this example we're just going to print it.
-
                 _logger.LogWarning(e, "Cannot register command {name}", command.Name);
             } catch (Exception e) {
                 _logger.LogError(e, "Error when creating command");
