62d2cab42b210f8628687e8a76244d714b6a9c65
Mathieu De Raedt <mathieu@deraedt.dev>
2021-09-04 15:59:22 +0200

    Delete unused code

--- Discord.Addons.Interactive/Criteria/ICriterion.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Threading.Tasks;
-using Discord.Commands;
-
-namespace Discord.Addons.Interactive.Criteria {
-    public interface ICriterion<in T> {
-        Task<bool> JudgeAsync(SocketCommandContext sourceContext, T parameter);
-    }
-}
--- Discord.Addons.Interactive/Results/OkResult.cs
+++
@@ -1,7 +0,0 @@
-﻿using Discord.Commands;
-
-namespace Discord.Addons.Interactive.Results {
-    public class OkResult : RuntimeResult {
-        public OkResult(string reason = null) : base(null, reason) { }
-    }
-}
