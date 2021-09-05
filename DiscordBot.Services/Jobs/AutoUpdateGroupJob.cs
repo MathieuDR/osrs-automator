2cf06d35b35a5600333765a476b42801c6576ade
Mathieu De Raedt <mathieu@deraedt.dev>
2021-09-05 22:15:43 +0200

    Fix job

--- DiscordBot.Services/Jobs/AutoUpdateGroupJob.cs
+++ DiscordBot.Services/Jobs/AutoUpdateGroupJob.cs
@@ -14,7 +14,7 @@ namespace DiscordBot.Services.Jobs {
     public class AutoUpdateGroupJob : ConfigurableGuildJob {
         private readonly IOsrsHighscoreService _osrsHighscoreService;
 
-        public AutoUpdateGroupJob(ILogger logger, IDiscordService discordService, IRepositoryStrategy repositoryStrategy, IOsrsHighscoreService osrsHighscoreService) : base(logger, discordService, JobType.GroupUpdate, repositoryStrategy) {
+        public AutoUpdateGroupJob(ILogger<AutoUpdateGroupJob> logger, IDiscordService discordService, IRepositoryStrategy repositoryStrategy, IOsrsHighscoreService osrsHighscoreService) : base(logger, discordService, JobType.GroupUpdate, repositoryStrategy) {
             _osrsHighscoreService = osrsHighscoreService;
         }
         protected override async Task<Result> DoWorkForGuildWithContext(Guild guild, GuildConfig guildConfig, ChannelJobConfiguration configuration) {
