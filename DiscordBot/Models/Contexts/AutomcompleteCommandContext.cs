070c63aad31c479e5d9145fae211d36231395097
Mathieu De Raedt <mathieu@deraedt.dev>
2021-11-16 14:23:19 +0100

    code cleanup

--- DiscordBot/Models/Contexts/AutomcompleteCommandContext.cs
+++ DiscordBot/Models/Contexts/AutomcompleteCommandContext.cs
@@ -1,5 +1,3 @@
-using Common;
-
 namespace DiscordBot.Models.Contexts;
 
 public class AutocompleteCommandContext : BaseInteractiveContext<SocketAutocompleteInteraction> {
@@ -11,16 +9,15 @@ public class AutocompleteCommandContext : BaseInteractiveContext<SocketAutocompl
 
     public string CommandName => InnerContext.Data.CommandName;
 
-    public string SubCommand => InnerContext.Data.Options.Where(x => x.Type == ApplicationCommandOptionType.SubCommand).Select(x => x.Name).FirstOrDefault();
+    public string SubCommand => InnerContext.Data.Options.Where(x => x.Type == ApplicationCommandOptionType.SubCommand).Select(x => x.Name)
+        .FirstOrDefault();
 
     public string CurrentOption => InnerContext.Data.Options.Where(x => x.Value == Current.Value).Select(x => x.Name).FirstOrDefault();
 
-    //public string Option => InnerContext.;
+    public string CommandFullName => string.IsNullOrWhiteSpace(SubCommand) ? CommandName : $"{CommandName} {SubCommand}";
+    public override string Message => $"Autocompleting '{Current}' for {CurrentOption} in {CommandFullName}";
 
     public Task RespondWithOptions<T>(IEnumerable<T> options) {
         return InnerContext.RespondAsync(options.Select(x => new AutocompleteResult(x.ToString(), x.ToString()?.ToLower())));
     }
-
-    public string CommandFullName => string.IsNullOrWhiteSpace(SubCommand) ? CommandName : $"{CommandName} {SubCommand}";
-    public override string Message => $"Autocompleting '{Current}' for {CurrentOption} in {CommandFullName}";
 }
