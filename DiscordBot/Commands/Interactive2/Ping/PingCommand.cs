7555d68885a834aea8b25030c8efec576d1f667e
Mathieu De Raedt <mathieu@deraedt.dev>
2021-11-23 12:40:24 +0100

    wip

---
+++ DiscordBot/Commands/Interactive2/Ping/PingCommand.cs
@@ -0,0 +1,19 @@
+﻿using DiscordBot.Commands.Interactive2.Interfaces;
+using DiscordBot.Common.Models.Enums;
+
+namespace DiscordBot.Commands.Interactive2.Ping; 
+
+public class PingCommand : ICommandDefinition {
+    public string Name { get; }
+    public string Description { get; }
+    public AuthorizationRoles MinimumAuthorizationRole { get; }
+    public Guid Id { get; }
+    
+    public Task<uint> GetCommandBuilderHash() {
+        throw new NotImplementedException();
+    }
+
+    public Task<SlashCommandProperties> GetCommandProperties() {
+        throw new NotImplementedException();
+    }
+}
