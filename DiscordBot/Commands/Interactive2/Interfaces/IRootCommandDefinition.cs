dee45a68935541ab2fd6e6232c0737e7ef65bbc6
Mathieu De Raedt <mathieu@deraedt.dev>
2021-11-24 00:16:12 +0100

    WIP

---
+++ DiscordBot/Commands/Interactive2/Interfaces/IRootCommandDefinition.cs
@@ -0,0 +1,36 @@
+﻿using DiscordBot.Common.Models.Enums;
+using MediatR;
+
+namespace DiscordBot.Commands.Interactive2.Interfaces;
+
+public interface IHandleableCommand<out TCommandDefinition, out TContext, TContextType> : IRequest<Result>
+    where TContext : BaseInteractiveContext<TContextType>
+    where TContextType : SocketInteraction
+    where TCommandDefinition : ICommandDefinition {
+    public AuthorizationRoles MinimumAuthorizationRole { get; }
+    public TContext Context { get; }
+
+    public TCommandDefinition CommandDefinition { get; }
+}
+
+public interface ICommandDefinition {
+    public string Name { get; }
+    public string Description { get; }
+}
+
+public interface IRootCommandDefinition : ICommandDefinition {
+    public Guid Id { get; }
+    Task<uint> GetCommandBuilderHash();
+    Task<SlashCommandProperties> GetCommandProperties();
+}
+
+public interface ISubCommandDefinition<TParentCommand> : ICommandDefinition
+    where TParentCommand : IRootCommandDefinition {
+    Task<SlashCommandOptionBuilder> GetOptionBuilder();
+}
+
+public interface ICommandHandler<in TCommand, TCommandDefinition, TContext, TContextType> : IRequestHandler<TCommand, Result>
+    where TCommand : IHandleableCommand<TCommandDefinition, TContext, TContextType>
+    where TContext : BaseInteractiveContext<TContextType>
+    where TContextType : SocketInteraction
+    where TCommandDefinition : ICommandDefinition { }
---
+++ DiscordBot/Commands/Interactive2/Ping/PingRootCommand.cs
@@ -0,0 +1,83 @@
+﻿using System.Text;
+using System.Text.Json;
+using DiscordBot.Commands.Interactive2.Interfaces;
+using HashDepot;
+
+namespace DiscordBot.Commands.Interactive2.Ping;
+
+public abstract class BaseSubCommand<TRoot> : ISubCommandDefinition<TRoot> where TRoot : IRootCommandDefinition {
+    public Task<SlashCommandOptionBuilder> GetOptionBuilder() {
+        throw new NotImplementedException();
+    }
+
+    public abstract string Name { get; }
+    public abstract string Description { get; }
+}
+
+public abstract class BaseRootCommand : IRootCommandDefinition {
+    public abstract Guid Id { get; }
+
+    public async Task<uint> GetCommandBuilderHash() {
+        var command = await GetCommandBuilder();
+        var json = JsonSerializer.Serialize(command);
+        var stream = Encoding.ASCII.GetBytes(json);
+        return XXHash.Hash32(stream);
+    }
+
+    public async Task<SlashCommandProperties> GetCommandProperties() {
+        var builder = await GetCommandBuilder();
+        return builder.Build();
+    }
+
+    public abstract string Name { get; }
+    public abstract string Description { get; }
+
+    public async Task<SlashCommandBuilder> GetCommandBuilder() {
+        var builder = new SlashCommandBuilder()
+            .WithName(Name)
+            .WithDescription(Description);
+
+        builder = await ExtendBaseSlashCommandBuilder(builder);
+
+        await AddSubCommands(builder);
+
+        return builder;
+    }
+
+    private async Task AddSubCommands(SlashCommandBuilder builder) {
+        var subs = await GetSubcommands();
+        var opts = await Task.WhenAll(subs.Select(x => x.GetOptionBuilder()));
+        builder.AddOptions(opts);
+    }
+
+    private async Task<IEnumerable<ISubCommandDefinition<BaseRootCommand>>> GetSubcommands() {
+        return await Task.FromResult(Array.Empty<ISubCommandDefinition<BaseRootCommand>>());
+    }
+
+    /// <summary>
+    ///     Extend the builder. The Name and description is already set
+    /// </summary>
+    /// <param name="builder">Builder with name and description set</param>
+    /// <returns>Fully build slash command builder</returns>
+    protected abstract Task<SlashCommandBuilder> ExtendBaseSlashCommandBuilder(SlashCommandBuilder builder);
+}
+
+public class PingRootCommand : BaseRootCommand, IRootCommandDefinition {
+    public override string Name => "ping2";
+    public override string Description => "Ping command through mediatr";
+
+    public override Guid Id => Guid.Parse("341A00F5-AB4A-451F-8FA4-639D54EE658C");
+
+    protected override Task<SlashCommandBuilder> ExtendBaseSlashCommandBuilder(SlashCommandBuilder builder) {
+        throw new NotImplementedException();
+    }
+}
+
+public class PingSubCommand : ISubCommandDefinition<PingRootCommand> {
+    public Task<SlashCommandOptionBuilder> GetOptionBuilder() {
+        throw new NotImplementedException();
+    }
+
+    public string Name { get; }
+    public string Description { get; }
+}
