b1a1a9df34e93fdf076119feeb03f8581c09a3ca
Mathieu De Raedt <mathieu@deraedt.dev>
2022-01-18 00:07:06 +0100

    start with GraveyardService.cs

---
+++ DiscordBot.Services/Interfaces/IGraveyardService.cs
@@ -0,0 +1,15 @@
+using DiscordBot.Common.Dtos.Discord;
+using DiscordBot.Common.Models.Data;
+using DiscordBot.Common.Models.Enums;
+using FluentResults;
+using WiseOldManConnector.Models.WiseOldMan.Enums;
+
+namespace DiscordBot.Services.Interfaces; 
+
+public interface IGraveyardService {
+	Task<Result> OptIn(GuildUser user);
+	Task<Result> OptOut(GuildUser user);
+	Task<Result> Shame(GuildUser user, ShameLocation location, string imageUrl, MetricType? metricType);
+	Task<Result<Shame>> GetShames(GuildUser user, ShameLocation? location, MetricType? metricTypeLocation);
+	Task<Result<Shame>> GetShames(Guild guild, ShameLocation? location, MetricType? metricTypeLocation);
+}
---
+++ DiscordBot.Services/Services/GraveyardService.cs
@@ -0,0 +1,7 @@
+using DiscordBot.Services.Interfaces;
+
+namespace DiscordBot.Services.Services; 
+
+internal class GraveyardService: IGraveyardService {
+	
+}
