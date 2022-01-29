d27cde07ad6ba3197668ee0805d285c4e5af10ba
Mathieu De Raedt <mathieu@deraedt.dev>
2022-01-29 02:07:15 +0100

    Add synonyms

--- DiscordBot/Commands/Interactive2/Graveyard/Shame/ShameAutoCompleteHandler.cs
+++ DiscordBot/Commands/Interactive2/Graveyard/Shame/ShameAutoCompleteHandler.cs
@@ -1,12 +1,19 @@
 using DiscordBot.Commands.Interactive2.Base.Handlers;
+using DiscordBot.Common.Configuration;
 using DiscordBot.Common.Models.Enums;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using WiseOldManConnector.Helpers;
 using WiseOldManConnector.Models.WiseOldMan.Enums;
 
 namespace DiscordBot.Commands.Interactive2.Graveyard.Shame;
 
 public class ShameAutoCompleteHandler : AutoCompleteHandlerBase<ShameAutoCompleteRequest> {
-	public ShameAutoCompleteHandler(IServiceProvider serviceProvider) : base(serviceProvider) { }
+	public ShameAutoCompleteHandler(IServiceProvider serviceProvider) : base(serviceProvider) {
+		var synonymsConfiguration = serviceProvider.GetRequiredService<IOptions<MetricSynonymsConfiguration>>().Value;
+
+		DefaultShameLocations = GetAllShameLocations(synonymsConfiguration);
+	}
 	protected override async Task<Result> DoWork(CancellationToken cancellationToken) {
 		_ = Context.RespondAsync(GetShameLocationOptions(Context.CurrentOptionAsString));
 		return Result.Ok();
@@ -14,22 +21,30 @@ public class ShameAutoCompleteHandler : AutoCompleteHandlerBase<ShameAutoComplet
 
 	private string[] GetShameLocationOptions(string start) {
 		var lowered = start.ToLowerInvariant();
-		return DefaultShameLocations.Where(x=> x.ToLowerInvariant().StartsWith(lowered)).ToArray();
+		return DefaultShameLocations.Where(x=> x.key.StartsWith(lowered)).Select(x=> x.value).Distinct().ToArray();
 	}
 
-	private string[] _defaultShameLocations = null;
-	private string[] DefaultShameLocations => _defaultShameLocations ??= GetAllShameLocations();
 
-	private string[] GetAllShameLocations() {
+	
+	private (string key, string value)[] DefaultShameLocations { get; }
+
+	private (string key, string value)[] GetAllShameLocations(MetricSynonymsConfiguration synonymsConfiguration) {
 		var metricTypeOptions = MetricTypeCategory.Skills.GetMetricTypes();
 		metricTypeOptions.AddRange(MetricTypeCategory.Bosses.GetMetricTypes());
 		metricTypeOptions.AddRange(MetricTypeCategory.Activities.GetMetricTypes());
 
 		var locations = new List<ShameLocation>() { ShameLocation.Wildy, ShameLocation.Other };
 		
-		var options = metricTypeOptions.Select(x => x.ToDisplayNameOrFriendly()).ToList();
-		options.AddRange(locations.Select(x => x.ToDisplayNameOrFriendly()));
+		var options = metricTypeOptions.Select(x => (x.ToDisplayNameOrFriendly().ToLowerInvariant(), x.ToDisplayNameOrFriendly())).ToList();
+		options.AddRange(locations.Select(x => (x.ToDisplayNameOrFriendly().ToLowerInvariant(), x.ToDisplayNameOrFriendly())));
 
-		return options.OrderBy(x=>x).ToArray();
+
+		foreach (var kvp in synonymsConfiguration.Synonyms) {
+			foreach (var synonym in kvp.Value) {
+				options.Add((synonym.ToLowerInvariant(), kvp.Key.ToDisplayNameOrFriendly()));
+			}
+		}
+
+		return options.OrderBy(x=>x.Item2).ToArray();
 	}
 }
