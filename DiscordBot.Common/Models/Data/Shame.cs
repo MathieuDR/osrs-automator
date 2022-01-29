7c431daea1e825d85821d7a4f4671697885c8ca0
Mathieu De Raedt <mathieu@deraedt.dev>
2022-01-30 00:05:37 +0100

    Update shame model

--- DiscordBot.Common/Models/Data/Shame.cs
+++ DiscordBot.Common/Models/Data/Shame.cs
@@ -6,16 +6,25 @@ namespace DiscordBot.Common.Models.Data;
 public record Shame {
 	public Shame() {
 		this.Id = Guid.NewGuid();
+		ShamedAt ??= DateTimeOffset.UtcNow;
 	}
 	
-	public Shame(ShameLocation location, MetricType? metricLocation, string imageUrl) : this() {
+	public Shame(ShameLocation location, MetricType? metricLocation, string imageUrl, ulong userId) : this() {
 		Location = location;
 		MetricLocation = metricLocation;
 		ImageUrl = imageUrl;
+		ShamedBy = userId;
+	}
+	
+	public Shame(ShameLocation location, MetricType? metricLocation, string imageUrl, ulong userId, DateTimeOffset shamedAt) : 
+		this(location, metricLocation, imageUrl, userId) {
+		ShamedAt = shamedAt;
 	}
 	
 	public Guid Id { get; }
+	public DateTimeOffset? ShamedAt { get; init; }
 	public ShameLocation Location { get; init; }
 	public MetricType? MetricLocation { get; init; }
+	public ulong ShamedBy { get; init; }
 	public string ImageUrl { get; init; }
 }
