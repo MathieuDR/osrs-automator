1f79bdef9c12008290ac6c87f4349df78e37dd61
Mathieu De Raedt <mathieu@deraedt.dev>
2022-01-31 00:44:23 +0100

    opt in

--- DiscordBot.Services/Services/GraveyardService.cs
+++ DiscordBot.Services/Services/GraveyardService.cs
@@ -1,17 +1,42 @@
 using DiscordBot.Common.Dtos.Discord;
 using DiscordBot.Common.Models.Data;
 using DiscordBot.Common.Models.Enums;
+using DiscordBot.Data.Interfaces;
 using DiscordBot.Services.Interfaces;
 using FluentResults;
+using Microsoft.Extensions.Logging;
 using WiseOldManConnector.Models.WiseOldMan.Enums;
 
 namespace DiscordBot.Services.Services; 
 
 internal class GraveyardService: IGraveyardService {
-	public GraveyardService() {
-		
+	private readonly IGraveyardRepository _graveyardRepository;
+	private readonly ILogger<GraveyardService> _logger;
+
+	public GraveyardService(IGraveyardRepository graveyardRepository, ILogger<GraveyardService> logger) {
+		_graveyardRepository = graveyardRepository;
+		_logger = logger;
 	}
-	public Task<Result> OptIn(GuildUser user) => throw new NotImplementedException();
+	
+	public Task<Result> OptIn(GuildUser user) {
+		_logger.LogInformation($"Opting in user {user.Username}");
+
+		var configurationResult = _graveyardRepository.GetSingle();
+		if (configurationResult.IsFailed) {
+			return Task.FromResult(configurationResult.ToResult());
+		}
+
+		Graveyard configuration = configurationResult.Value ?? new Graveyard();
+		
+		if (configuration.OptedInUsers.Any(x => x == user.Id)) {
+			return Task.FromResult(Result.Ok());
+		}
+		
+		configuration.OptedInUsers.Add(user.Id);
+		var repoResult = _graveyardRepository.UpdateOrInsert(configuration);
+		
+		return Task.FromResult(Result.OkIf(repoResult.IsSuccess, "Could not opt in").WithErrors(repoResult.Errors));
+	}
 
 	public Task<Result> OptOut(GuildUser user) => throw new NotImplementedException();
 	public Task<Result<bool>> IsOptedIn(GuildUser user) => throw new NotImplementedException();
