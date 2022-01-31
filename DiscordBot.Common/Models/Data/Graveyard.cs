5694cc684bdb53e4fb1767fa1dedf25b60dc3143
Mathieu De Raedt <mathieu@deraedt.dev>
2022-01-31 03:29:21 +0100

    auto init dict

--- DiscordBot.Common/Models/Data/Graveyard.cs
+++ DiscordBot.Common/Models/Data/Graveyard.cs
@@ -4,5 +4,5 @@ namespace DiscordBot.Common.Models.Data;
 
 public record Graveyard : BaseGuildRecord {
 	public List<ulong> OptedInUsers { get; set; } = new List<ulong>();
-	public Dictionary<ulong, List<Shame>> Shames { get; set; }
+	public Dictionary<ulong, List<Shame>> Shames { get; set; } = new Dictionary<ulong, List<Shame>>();
 }
