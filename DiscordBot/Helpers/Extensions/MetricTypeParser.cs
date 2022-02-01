2dc9481be8044e8beb805b71abe10071c75a04a9
Mathieu De Raedt <mathieu@deraedt.dev>
2022-02-01 01:48:50 +0100

    Reworked metric type parser to work with attributes

--- DiscordBot/Helpers/Extensions/MetricTypeParser.cs
+++ DiscordBot/Helpers/Extensions/MetricTypeParser.cs
@@ -1,34 +1,55 @@
-﻿using DiscordBot.Common.Configuration;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Runtime.Serialization;
+using DiscordBot.Common.Configuration;
 using Microsoft.Extensions.Options;
 using WiseOldManConnector.Models.WiseOldMan.Enums;
 
 namespace DiscordBot.Helpers.Extensions;
 
 public class MetricTypeParser {
-    private readonly IOptions<MetricSynonymsConfiguration> _options;
+	private readonly IOptions<MetricSynonymsConfiguration> _options;
 
-    public MetricTypeParser(IOptions<MetricSynonymsConfiguration> options) {
-        _options = options;
-    }
+	public MetricTypeParser(IOptions<MetricSynonymsConfiguration> options) => _options = options;
 
-    public bool TryParseToMetricType(string metricType, out MetricType value) {
-        var configuration = _options.Value;
+	public bool TryParseToMetricType(string metricType, out MetricType value) {
+		var configuration = _options.Value;
+		value = 0; // default
 
-        if (Enum.TryParse(metricType, true, out value)) {
-            return true;
-        }
+		// Parse by attributes
+		var loweredInput = metricType.ToLowerInvariant();
 
-        if (configuration == null || configuration.Synonyms == null || configuration.Synonyms.Count <= 1) {
-            return false;
-        }
+		foreach (var field in typeof(MetricType).GetFields()) {
+			var displayAttr = field.GetCustomAttribute<DisplayAttribute>();
 
-        foreach (var kvp in configuration.Synonyms) {
-            if (kvp.Value.Contains(metricType, StringComparer.InvariantCultureIgnoreCase)) {
-                value = kvp.Key;
-                return true;
-            }
-        }
+			if (displayAttr is not null && displayAttr.Name is not null && displayAttr.Name.ToLowerInvariant() == loweredInput) {
+				value = (MetricType)field.GetValue(null);
+				return true;
+			}
 
-        return false;
-    }
+			var enumMemAttr = field.GetCustomAttribute<EnumMemberAttribute>();
+			if (enumMemAttr is not null && enumMemAttr.Value is not null && enumMemAttr.Value.ToLowerInvariant() == loweredInput) {
+				value = (MetricType)field.GetValue(null);
+				return true;
+			}
+			
+			if(field.Name.ToLowerInvariant() == loweredInput) {
+				value = (MetricType)field.GetValue(null);
+				return true;
+			}
+		}
+
+		if (configuration == null || configuration.Synonyms == null || configuration.Synonyms.Count <= 1) {
+			return false;
+		}
+
+		foreach (var kvp in configuration.Synonyms) {
+			if (kvp.Value.Contains(metricType, StringComparer.InvariantCultureIgnoreCase)) {
+				value = kvp.Key;
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
