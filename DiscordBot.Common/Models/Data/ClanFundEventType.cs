7cd5a45c6d88e0f8db23eb961b860808809792ac
Mathieu De Raedt <mathieu@deraedt.dev>
2022-02-12 00:59:28 +0100

    Clanfunds models, repo's and services

---
+++ DiscordBot.Common/Models/Data/ClanFundEventType.cs
@@ -0,0 +1,11 @@
+namespace DiscordBot.Common.Models.Data;
+
+public enum ClanFundEventType {
+	// fund event types
+	Deposit,
+	Withdraw,
+	Donation,
+	Refund,
+	Other,
+	System
+}
---
+++ DiscordBot.Services/Interfaces/IClanFundsService.cs
@@ -0,0 +1,13 @@
+using DiscordBot.Common.Dtos.Discord;
+using DiscordBot.Common.Models.Data;
+using FluentResults;
+
+namespace DiscordBot.Services.Interfaces; 
+
+public interface IClanFundsService {
+	Task<Result<IEnumerable<ClanFundEvent>>> GetClanFundEvents(Guild guild);
+	Task<Result<ClanFunds>> GetClanFund(Guild guild);
+	Task<Result> AddClanFund(Guild guild, ClanFundEvent clanFundEvent);
+	
+	Task<Result> Initialize(Guild guild, Channel trackingChannel, Channel donationChannel, long? currentFunds = null);
+}
