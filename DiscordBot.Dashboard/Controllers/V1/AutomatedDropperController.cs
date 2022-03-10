6b16ed2dc9c277a82ac9975d79561e34ab2bc10c
Mathieu De Raedt <mathieu@deraedt.dev>
2022-03-11 00:01:03 +0100

    dashboard buildable

--- DiscordBot.Dashboard/Controllers/V1/AutomatedDropperController.cs
+++ DiscordBot.Dashboard/Controllers/V1/AutomatedDropperController.cs
@@ -3,6 +3,7 @@ using Dashboard.Binders;
 using Dashboard.Models.ApiRequests.DiscordEmbed;
 using Dashboard.Transformers;
 using DiscordBot.Common.Dtos.Runescape;
+using DiscordBot.Common.Identities;
 using DiscordBot.Services.Interfaces;
 using FluentResults;
 using Microsoft.AspNetCore.Mvc;
@@ -13,61 +14,59 @@ namespace Dashboard.Controllers.V1;
 [ApiVersion("1.0")]
 [Route("api/v{version:apiVersion}/[controller]")]
 public class AutomatedDropperController : Controller {
-    private readonly IAutomatedDropperService _dropperService;
-    private readonly ILogger<AutomatedDropperController> _logger;
-    private readonly IMapper<Embed, RunescapeDrop> _mapper;
+	private readonly IAutomatedDropperService _dropperService;
+	private readonly ILogger<AutomatedDropperController> _logger;
+	private readonly IMapper<Embed, RunescapeDrop> _mapper;
 
-    public AutomatedDropperController(ILogger<AutomatedDropperController> logger, IMapper<Embed, RunescapeDrop> mapper,
-        IAutomatedDropperService dropperService) {
-        _logger = logger;
-        _mapper = mapper;
-        _dropperService = dropperService;
-    }
+	public AutomatedDropperController(ILogger<AutomatedDropperController> logger, IMapper<Embed, RunescapeDrop> mapper,
+		IAutomatedDropperService dropperService) {
+		_logger = logger;
+		_mapper = mapper;
+		_dropperService = dropperService;
+	}
 
-    [HttpPost("dropper/{id:required}")]
-    public async Task<IActionResult> Get([FromBody] EmbedCollection bodyEmbeds,
-        [ModelBinder(BinderType = typeof(JsonModelBinder))]
-        EmbedCollection formEmbeds,
-        [FromForm] IFormFile file, [FromRoute] string id) {
+	[HttpPost("dropper/{id:required}")]
+	public async Task<IActionResult> Get([FromBody] EmbedCollection bodyEmbeds,
+		[ModelBinder(BinderType = typeof(JsonModelBinder))]
+		EmbedCollection formEmbeds,
+		[FromForm] IFormFile? file, [FromRoute] string id) {
+		if (!ulong.TryParse(id, out var endpoint)) {
+			BadRequest("Invalid endpoint");
+		}
 
-        if (!ulong.TryParse(id, out var endpoint)) {
-            BadRequest("Invalid endpoint");
-        }
-        
-        _logger.LogInformation("Received drop");
-        var dropResult = GetDrop(bodyEmbeds, formEmbeds);
-        if (dropResult.IsFailed) {
-            _logger.LogError("Error with receiving result: {0}",dropResult.CombineMessage());
-            BadRequest(dropResult.Errors.FirstOrDefault());
-        }
+		_logger.LogInformation("Received drop");
+		var dropResult = GetDrop(bodyEmbeds, formEmbeds);
+		if (dropResult.IsFailed) {
+			_logger.LogError("Error with receiving result: {0}", dropResult.CombineMessage());
+			BadRequest(dropResult.Errors.FirstOrDefault());
+		}
 
-        var image = await ToBase64String(file);
-        _ = _dropperService.HandleDropRequest(endpoint, dropResult.Value, image);
+		string? image = null;
+		if (file is not null) {
+			image = await ToBase64String(file);
+		}
 
-        return Ok();
-    }
+		_ = _dropperService.HandleDropRequest(new DiscordUserId(endpoint), dropResult.Value, image);
 
-    private Result<RunescapeDrop> GetDrop(EmbedCollection bodyEmbeds, EmbedCollection formEmbeds) {
-        var embeds = bodyEmbeds ?? formEmbeds;
-        var embed = embeds?.Embeds.FirstOrDefault();
+		return Ok();
+	}
 
-        if (embed is not null) {
-            return _mapper.Map(embed);
-        }
+	private Result<RunescapeDrop> GetDrop(EmbedCollection bodyEmbeds, EmbedCollection formEmbeds) {
+		var embeds = bodyEmbeds ?? formEmbeds;
+		var embed = embeds?.Embeds.FirstOrDefault();
 
-        return Result.Ok();
-    }
+		if (embed is not null) {
+			return _mapper.Map(embed);
+		}
 
-    private static async Task<string> ToBase64String(IFormFile file) {
-        string result = null;
-        if (file is not null) {
-            await using var stream = file.OpenReadStream();
-            await using var memoryStream = new MemoryStream();
-            await stream.CopyToAsync(memoryStream);
-            var bytes = memoryStream.ToArray();
-            result = Convert.ToBase64String(bytes);
-        }
+		return Result.Ok();
+	}
 
-        return result;
-    }
+	private static async Task<string> ToBase64String(IFormFile file) {
+		await using var stream = file.OpenReadStream();
+		await using var memoryStream = new MemoryStream();
+		await stream.CopyToAsync(memoryStream);
+		var bytes = memoryStream.ToArray();
+		return Convert.ToBase64String(bytes);
+	}
 }
