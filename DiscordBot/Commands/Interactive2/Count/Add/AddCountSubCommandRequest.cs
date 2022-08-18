8d3c5d66c9656b2081a7d4f4bd2024f1f170fa6f
Mathieu De Raedt <mathieu@deraedt.dev>
2022-08-18 19:55:25 +0200

    Revert authorization level

--- DiscordBot/Commands/Interactive2/Count/Add/AddCountSubCommandRequest.cs
+++ DiscordBot/Commands/Interactive2/Count/Add/AddCountSubCommandRequest.cs
@@ -4,5 +4,5 @@ namespace DiscordBot.Commands.Interactive2.Count.Add;
 
 public class AddCountSubCommandRequest : ApplicationCommandRequestBase<AddCountSubCommandDefinition> {
     public AddCountSubCommandRequest(ApplicationCommandContext context) : base(context) { }
-    public override AuthorizationRoles MinimumAuthorizationRole => AuthorizationRoles.ClanModerator;
+    public override AuthorizationRoles MinimumAuthorizationRole => AuthorizationRoles.ClanEventHost;
 }
